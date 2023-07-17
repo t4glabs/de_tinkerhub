bccbbd58b93dc97f7604ab3f1fb74e452ef9491e
Aysha PP <ayshapp@Ayshas-MacBook-Air.local>
2023-07-16 22:32:05 -0700

    add past-events portal page

---
+++ de_tinkerhub/www/past-events/index.py
@@ -0,0 +1,27 @@
+import frappe
+
+cur_user = frappe.session.user
+
+def get_context(context):
+
+    if cur_user=='Guest':
+        frappe.throw( ("You have to login as a member access this page"), frappe.PermissionError)
+    else:
+        participated_events = frappe.get_all(
+            'Event Participant',
+            filters={
+                'participant_email': cur_user
+            },
+            fields=['event_id'],
+            as_list=True 
+        )
+        event_ids = [event[0] for event in participated_events]
+
+        context.events = frappe.db.get_list('TinkerHub Event',
+                                    filters={
+                                        'name': ['in', event_ids]
+                                    },
+                                    fields=['name','title', 'date'])
+
+    return context
+
