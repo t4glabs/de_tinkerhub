2ba85af2d375ea01368a8bae4219acab175ffd4c
Aysha PP <ayshapp@Ayshas-MacBook-Air.local>
2023-07-25 11:45:19 -0700

    edits

--- de_tinkerhub/www/upcoming-events/index.py
+++ de_tinkerhub/www/upcoming-events/index.py
@@ -6,7 +6,7 @@ def get_context(context):
     today = datetime.date.today()
     context.events = frappe.db.get_list('TinkerHub Event',
                                 filters={
-                                    'starting_date':  (">", today),
+                                    'starting_date':  [">=", today],
                                     'status': 'Confirmed',
                                     'is_published': 1
                                 },
