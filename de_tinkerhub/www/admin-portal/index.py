b287f5ed73aa0123c2884e93268635eadae0895f
Aysha PP <ayshapp@Ayshas-MacBook-Air.local>
2023-09-29 10:48:20 -0700

    event portal

--- de_tinkerhub/www/admin-portal/index.py
+++ de_tinkerhub/www/admin-portal/index.py
@@ -1,45 +1,54 @@
 import frappe
 
+no_cache = 1
+no_sitemap = 1
+
 roles = frappe.get_roles()
 cur_user = frappe.session.user
 
-if  (cur_user == 'Guest' or 'Event Admin' not in roles) and cur_user != 'Administrator':
-    frappe.throw( ("You have to login as an Event Admin to access this page"),frappe.PermissionError)
-    event_admin = False
-else: 
-    def get_context(context):
 
-        event_admin = True
+def get_context(context):
 
-        college = frappe.db.get_value('Learner', cur_user, 'college')
+    if  (cur_user == 'Guest' or 'Event Admin' not in roles) and cur_user != 'Administrator':
+        event_admin = False
+        frappe.throw( ("You have to login as an Event Admin to access this page"),frappe.PermissionError)
+        
 
-        if college:
-           admins = frappe.db.get_list('Learner',
-                               filters={
-                                   'college': college
-                               },
-                               fields=['email'],
-                               as_list=True 
-                               )
-           college_admins = [admin[0] for admin in admins ]
-           
-           context.events = frappe.db.get_list('TinkerHub Event',
-                                    filters = {
-                                        'owner': ['in', college_admins]
-                                    },
-                                    fields=['name','title', 'starting_date']
-                                    )
-        else:
-            context.events = frappe.db.get_list('TinkerHub Event',
-                                    filters={
-                                        'owner':  cur_user
-                                    },
-                                    fields=['name','title', 'starting_date'])
-           
-        context.show_sidebar = 1
-        context.no_cache = 1
-        context.event_admin = event_admin
-        return context
+        # context.no_cache = 1
+        # context.no_sitemap = 1
+    
+    event_admin = True
+
+    
+
+    college = frappe.db.get_value('Learner', cur_user, 'college')
+
+    if college:
+        admins = frappe.db.get_list('Learner',
+                            filters={
+                                'college': college
+                            },
+                            fields=['email'],
+                            as_list=True 
+                            )
+        college_admins = [admin[0] for admin in admins ]
+        
+        context.events = frappe.db.get_list('TinkerHub Event',
+                                filters = {
+                                    'owner': ['in', college_admins]
+                                },
+                                fields=['name','title', 'starting_date']
+                                )
+    else:
+        context.events = frappe.db.get_list('TinkerHub Event',
+                                filters={
+                                    'owner':  cur_user
+                                },
+                                fields=['name','title', 'starting_date'])
+        
+    context.show_sidebar = 1
+    context.event_admin = event_admin
+    return context
 
 
 
